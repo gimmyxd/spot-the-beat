6a8d975fae83129aa12a31d73487c1291fd4032c
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2018-05-11 21:46:36 +0300

    added dashboard and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'dashboard', to: 'dashboard#index'
+  root 'dashboard#index'
 end
