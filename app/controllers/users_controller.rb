c0d5dbf643eeb3483689ad691cbffe499b98dd24
vlad ilie <vlad.ilie@unifiedpost.com>
2018-05-12 08:52:27 +0300

    Show the image of artists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
     # Access private data
     @spotify_user.country #=> "US"
     Rails.logger.info @spotify_user.to_hash
+    #
+    # user = User.new do |u|
+    #   u.email = @spotify_user.email   #=> "example@email.com"
+    # end
+    # user.save
 
-    user = User.new do |u|
-      u.email = @spotify_user.email   #=> "example@email.com"
-    end
-    user.save
+    Rails.logger.info(@spotify_user.top_artists.first.images.first['url'])
 
     render "dashboard/index"
   end
