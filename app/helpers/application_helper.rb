5b9752f4da387328c032d65b066ade579ae5c804
Anca <anca.rosu@unifiedpost.com>
2018-05-13 12:29:16 +0300

    Improved message cases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def events_present(events)
+    !spotify_events_empty(events) || filtered_events_present(events)
+  end
+
+  def spotify_events_empty(events)
+    return false if events.length > 1
+    artists_with_no_events = events.values.select  { |value| value.empty? }
+    artists_with_no_events.count == events.count
+  end
+
+  def filtered_events_present(events)
+    events["1"].present?
+  end
 end
